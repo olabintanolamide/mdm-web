5d091183613b8ce0db4dd9c4a0b330780bb47a00
olamide <olabintanolamide145@gmail.com>
2022-07-19 11:56:30 +0100

    setting up dashboard

---
+++ src/modules/admin/views/Dashboard/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { TextField, Box } from "@mui/material";
+
+const Dashboard = () => {
+  return(
+   <Box>
+     <TextField>
+        
+     </TextField>
+     fshgdhgasjdbfjbdsjf
+   </Box>
+  
+  )
+  
+};
+
+export default Dashboard;
