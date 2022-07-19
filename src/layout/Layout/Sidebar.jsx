108d0688be4d24f08d64d72b9406cdb873f5c97d
olamide <olabintanolamide145@gmail.com>
2022-07-19 13:49:06 +0100

    side and topbar implementation

--- src/layout/Layout/Sidebar.jsx
+++ src/layout/Layout/Sidebar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Drawer } from '@mui/material'
 
 const Sidebar = props => {
   return (
-    <div>Sidebar</div>
+    <Drawer variant='persistent' anchor='left' open
+        sx={{
+            width: 240,
+            "& .MuiDrawer-paper" : {
+                width: 240
+            }
+        }}
+    >
+        <div>
+            testing the drawer microphone 
+        </div>
+
+    </Drawer>
   )
 }
 
